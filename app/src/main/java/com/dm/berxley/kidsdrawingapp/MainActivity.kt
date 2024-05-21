7dc5a29e88da9565531b4b74e4240701d0b24e98
Dennis Muoki <muokid3@gmail.com>
2024-05-21 18:07:51 +0300

    color pallet with custom drawables

--- app/src/main/java/com/dm/berxley/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/dm/berxley/kidsdrawingapp/MainActivity.kt
@@ -4,16 +4,28 @@ import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
     private var drawing_view:DrawingView? = null
+    private var mImageButtonCurrentpaint: ImageButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         drawing_view = findViewById(R.id.drawing_view)
         drawing_view?.setSizeForBrush(20.toFloat())
+
+        val linearLayout: LinearLayout = findViewById(R.id.ll_paint_colors)
+
+        mImageButtonCurrentpaint = linearLayout[1] as ImageButton
+        mImageButtonCurrentpaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+        )
 
         val ib_brush: ImageButton = findViewById(R.id.ib_brush)
 
